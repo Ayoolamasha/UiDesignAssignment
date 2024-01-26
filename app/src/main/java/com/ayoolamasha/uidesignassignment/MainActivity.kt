ed79bc14e754511106795c6243e135c7f3815a7c
ayoolamasha@gmail.com <ayoolamasha@gmail.com>
2024-01-26 20:07:46 +0100

    init
    Project created

---
+++ app/src/main/java/com/ayoolamasha/uidesignassignment/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ayoolamasha.uidesignassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.TransitionManager
+import com.google.android.material.badge.BadgeDrawable
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
